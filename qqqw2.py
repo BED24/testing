831ffe657dec36bfb8c84e4b3dfd6968b075903b
ANDREY <texnik64@iclou.com>
2024-10-06 18:24:51 +0300

    yariables

---
+++ qqqw2.py
@@ -0,0 +1,26 @@
+import turtle as t # подключили "черепашку"
+
+#RAM - Random access memory
+sides = 8 #Число сторон угла
+dist = 120 # переменной dist присвоенрное значение 150
+corner = 360 / sides # Угол
+
+
+t.forward(dist)
+t.right(corner)
+t.forward(dist)
+t.right(corner)
+t.forward(dist)
+t.right(corner)
+t.forward(dist)
+t.right(corner)
+t.forward(dist)
+t.right(corner)
+t.forward(dist)
+t.right(corner)
+t.forward(dist)
+t.right(corner)
+t.forward(dist)
+t.right(corner)
+
+t.mainloop()
